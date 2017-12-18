876319b28bb9b1b062c7bc73084663cd31704c2d
Umut <umutsahin@protonmail.com>
2017-12-18 16:28:51 +0300

    chore(rollup): Comments added to rollup.config.js

--- README.md
+++ README.md
@@ -3,8 +3,10 @@ A Linear Doubly Linked List implementation with Array like prototype
 
 [![License](https://img.shields.io/npm/l/linist.svg)](http://opensource.org/licenses/MIT)
 [![Version](https://img.shields.io/npm/v/linist.svg)](http://npm.im/linist)
-[![NSP Status](https://nodesecurity.io/orgs/linist/projects/16525912-3f0e-4a32-a965-c2bfa58c230f/badge)](https://nodesecurity.io/orgs/linist/projects/16525912-3f0e-4a32-a965-c2bfa58c230f)
 [![Maintainability](https://api.codeclimate.com/v1/badges/89720625c0392ae435d8/maintainability)](https://codeclimate.com/github/umut-sahin/linist/maintainability)
 [![Travis Build Status](https://img.shields.io/travis/umut-sahin/linist.svg)](https://travis-ci.org/umut-sahin/linist)
 [![Codecov Coverage Report](https://img.shields.io/codecov/c/github/umut-sahin/linist.svg)](https://codecov.io/gh/umut-sahin/linist)
+[![Dependencies Status](https://david-dm.org/umut-sahin/linist/status.svg)](https://david-dm.org/umut-sahin/linist)
+[![DevDependencies Status](https://david-dm.org/umut-sahin/linist/dev-status.svg)](https://david-dm.org/umut-sahin/linist?type=dev)
+[![NSP Status](https://nodesecurity.io/orgs/linist/projects/16525912-3f0e-4a32-a965-c2bfa58c230f/badge)](https://nodesecurity.io/orgs/linist/projects/16525912-3f0e-4a32-a965-c2bfa58c230f)
 [![Semantic Release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
--- rollup.config.js
+++ rollup.config.js
@@ -1,83 +1,233 @@
-import uglify from "rollup-plugin-uglify";
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import babel from "rollup-plugin-babel";
-import {
-  minify
-} from "uglify-es";
+// #region Import plugins
+
+import uglifyPlugin from "rollup-plugin-uglify";
+
+import resolvePlugin from "rollup-plugin-node-resolve";
+
+import commonjsPlugin from "rollup-plugin-commonjs";
+
+import babelPlugin from "rollup-plugin-babel";
+
+import uglifyEs from "uglify-es";
+
+// #endregion
+
+// #region Specify entry point
 
 const input = "src/Linist.js";
 
-const output = [{
-    file: "dist/Linist.js",
-    format: "umd",
-    name: "Linist",
-    sourcemap: true
-  }, {
-    file: "dist/Linist.mjs",
-    format: "es",
-    name: "Linist",
-    sourcemap: true
-  },
+// #endregion
+
+// #region Specify outputs
+
+const developmentOutput = [
+
+  // UMD format
   {
-    file: "dist/Linist.esm.js",
-    format: "es",
+
+    file: "dist/Linist.js",
+
+    format: "umd",
+
     name: "Linist",
+
     sourcemap: true
+
+  },
+
+  // Michael jackson script format
+  {
+
+    file: "dist/Linist.mjs",
+
+    format: "es",
+
+    name: "Linist",
+
+    sourcemap: true
+
+  },
+
+  // ECMAScript 6 module format
+  {
+
+    file: "dist/Linist.esm.js",
+
+    format: "es",
+
+    name: "Linist",
+
+    sourcemap: true
+
   }
+
 ];
 
-const resolveDefault = resolve();
-const commonjsDefault = commonjs();
-const babelDefault = babel({
-  presets: [
-    [
-      "env", {
-        "modules": false
-      }
-    ]
-  ],
-  plugins: [
-    "external-helpers"
-  ],
-  babelrc: false,
-  exclude: "node_modules/**"
-});
-const uglifyDefault = uglify({
-  "keep_classnames": true,
-  "keep_fnames": true,
-  "ecma": 6,
-  "mangle": true,
-  "compress": {
-    "arrows": true,
-    "sequences": true,
-    "dead_code": true,
-    "conditionals": true,
-    "booleans": true,
-    "unused": true,
-    "if_return": true,
-    "join_vars": true,
-    "drop_console": true,
-    "cascade": true,
-    "comparisons": true
-  }
-}, minify);
+const productionOutput = [
 
-export default [{
-  input,
-  output,
+  // UMD format
+  {
+
+    file: "dist/Linist.min.js",
+
+    format: "umd",
+
+    name: "Linist",
+
+    sourcemap: true
+
+  },
+
+  // Michael jackson script format
+  {
+
+    file: "dist/Linist.min.mjs",
+
+    format: "es",
+
+    name: "Linist",
+
+    sourcemap: true
+
+  },
+
+  // ECMAScript 6 module format
+  {
+
+    file: "dist/Linist.esm.min.js",
+
+    format: "es",
+
+    name: "Linist",
+
+    sourcemap: true
+
+  }
+
+];
+
+// #endregion
+
+// #region Initialize plugins
+
+const uglify = uglifyPlugin({
+
+  "keep_classnames": true,
+
+  "keep_fnames": true,
+
+  "ecma": 6,
+
+  "mangle": true,
+
+  "compress": {
+
+    "arrows": true,
+
+    "sequences": true,
+
+    "dead_code": true,
+
+    "conditionals": true,
+
+    "booleans": true,
+
+    "unused": true,
+
+    "if_return": true,
+
+    "join_vars": true,
+
+    "drop_console": true,
+
+    "cascade": true,
+
+    "comparisons": true
+
+  }
+
+}, uglifyEs.minify);
+
+const resolve = resolvePlugin();
+
+const commonjs = commonjsPlugin();
+
+const babel = babelPlugin({
+
+  babelrc: false,
+
+  presets: [
+
+    [
+
+      "env",
+
+      {
+
+        "modules": false
+
+      }
+
+    ]
+
+  ],
+
   plugins: [
-    resolveDefault,
-    commonjsDefault,
-    babelDefault
-  ]
-}, {
-  input,
-  output,
-  plugins: [
-    uglifyDefault,
-    resolveDefault,
-    commonjsDefault,
-    babelDefault
-  ]
-}];
+
+    "external-helpers"
+
+  ],
+
+  exclude: "node_modules/**"
+
+});
+
+// #endregion
+
+// #region Export configuration
+
+export default [
+
+  // Non-Minified development distribution
+  {
+
+    input,
+
+    output: developmentOutput,
+
+    plugins: [
+
+      resolve,
+
+      commonjs,
+
+      babel
+
+    ]
+
+  },
+
+  // Minified production distribution
+  {
+
+    input,
+
+    output: productionOutput,
+
+    plugins: [
+
+      uglify,
+
+      resolve,
+
+      commonjs,
+
+      babel
+
+    ]
+
+  }
+
+];
+
+// #endregion
